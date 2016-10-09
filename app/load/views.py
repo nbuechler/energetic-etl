1d5da4064f94c9d14a5d7cf1075aadf4898a7970
nbuechler <natebuechler@gmail.com>
2016-10-08 20:22:46 -0400

    added endpoints for extracting nodes

--- app/load/views.py
+++ app/load/views.py
@@ -12,60 +12,60 @@ def default():
 '''
 Activities
 '''
-@load.route('/mongo2neo/create_single_activity/<activity>', methods=['POST'])
+@load.route('/create_single_activity/<activity>', methods=['POST'])
 def create_single_activity(activity=None):
     return controllers.create_single_activity(activity=None)
 
-@load.route('/mongo2neo/update_single_activity/<activity>', methods=['PUT'])
+@load.route('/update_single_activity/<activity>', methods=['PUT'])
 def update_single_activity(activity=None):
     return controllers.update_single_activity(activity=None)
 
-@load.route('/mongo2neo/destroy_single_activity/<activity>', methods=['DELETE'])
+@load.route('/destroy_single_activity/<activity>', methods=['DELETE'])
 def destroy_single_activity(activity=None):
     return controllers.destroy_single_activity(activity=None)
 
 '''
 Experiences
 '''
-@load.route('/mongo2neo/create_single_experience/<experience>', methods=['POST'])
+@load.route('/create_single_experience/<experience>', methods=['POST'])
 def create_single_experience(experience=None):
     return controllers.create_single_experience(experience=None)
 
-@load.route('/mongo2neo/update_single_experience/<experience>', methods=['PUT'])
+@load.route('/update_single_experience/<experience>', methods=['PUT'])
 def update_single_experience(experience=None):
     return controllers.update_single_experience(experience=None)
 
-@load.route('/mongo2neo/destroy_single_experience/<experience>', methods=['DELETE'])
+@load.route('/destroy_single_experience/<experience>', methods=['DELETE'])
 def destroy_single_experience(experience=None):
     return controllers.destroy_single_experience(experience=None)
 
 '''
 Logs
 '''
-@load.route('/mongo2neo/create_single_log/<log>', methods=['POST'])
+@load.route('/create_single_log/<log>', methods=['POST'])
 def create_single_log(log=None):
     return controllers.create_single_log(log=None)
 
-@load.route('/mongo2neo/update_single_log/<log>', methods=['PUT'])
+@load.route('/update_single_log/<log>', methods=['PUT'])
 def update_single_log(log=None):
     return controllers.update_single_log(log=None)
 
-@load.route('/mongo2neo/destroy_single_log/<log>', methods=['DELETE'])
+@load.route('/destroy_single_log/<log>', methods=['DELETE'])
 def destroy_single_log(log=None):
     return controllers.destroy_single_log(log=None)
 
 '''
 General Record Keeping
 '''
-@load.route('/mongo2neo/delete_records')
+@load.route('/delete_records')
 def delete_records():
     return controllers.delete_records()
 
-@load.route('/mongo2neo/create_records')
+@load.route('/create_records')
 def create_records():
     return controllers.create_records()
 
-@load.route('/mongo2neo/create_event_supplement')
+@load.route('/create_event_supplement')
 def create_event_supplement():
     # TODO: Make this method execute differently if there is a user_id
     # For example, if a user_id, make it so that user has updated event nodes
