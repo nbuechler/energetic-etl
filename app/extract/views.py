8a599f5e21551a09c1310470e4eededf32ee0eac
nbuechler <natebuechler@gmail.com>
2016-10-20 23:45:37 -0400

    made the endpoint work out better for similarity checking between two orders

--- app/extract/views.py
+++ app/extract/views.py
@@ -57,14 +57,35 @@ def get_rep_emotion_all(rEmotion=None, order_num=None):
 Affect-Word Similarty
 '''
 
-# TODO: Think about a better developer experience for an api!
-@extract.route('/emotion/<rEmotion>/comparison/<order_A>/with/<order_B>/for/<word>')
+'''
+rEmotion are strings
+order_A are numbers
+order_B are numbers
+word are strings
+--
+Returns 1 if word exists in both order_A and order_B
+Returns 0 if word does NOT exist in both order_A and order_B
+'''
+@extract.route('/emotion/<rEmotion>/order/<order_A>,<order_B>/<word>')
 def check_similarity_for_rep_emotion(rEmotion=None, order_A=None, order_B=None, word=None):
-    result = controllers.check_similarity_for_rep_emotion(order_A=order_A, order_B=order_B, rEmotion=rEmotion, word=word)
-    print result
-    return 'Barely Implemented'
+    query_result = controllers.check_similarity_for_rep_emotion(order_A=order_A, order_B=order_B, rEmotion=rEmotion, word=word)
 
-@extract.route('/emotion/<rEmotion>/comparison/allfor/<word>')
+    result = 0
+    try:
+        word_total = query_result[0][1]
+        if word_total == 1:
+            result = 0
+        elif word_total == 2:
+            result = 1
+    except Exception as e:
+        # TODO: Write the exception of index out of bounds for: query_result[0][1]
+        # to a log file
+        print 'Word does not exist in either order'
+        pass
+
+    return str(result)
+
+@extract.route('/emotion/<rEmotion>/order/all/<word>')
 def check_o1_o2_o3_similarity_for_rep_emotion(rEmotion=None, word=None):
     result = controllers.check_o1_o2_o3_similarity_for_rep_emotion(rEmotion=rEmotion, word=word)
     print result
